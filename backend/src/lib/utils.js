bd9953f0e20bec195d566284ad1e5a97c6f1219b
Nathan <87546582+nathandallas@users.noreply.github.com>
2025-07-19 12:06:44 -0400

    completed signup auth

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, `${process.env.JWT_SECRET_KEY}`, { expiresIn: "7d" });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true, // Prevents XXS attacks and client-side JavaScript from accessing the cookie
+    sameSite: "strict", // Helps prevent CSRF attacks
+    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+  });
+  return token;
+};
