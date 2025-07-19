7bf0a3538f6ccbf35e85d25426129b7c69ffe199
Nathan <87546582+nathandallas@users.noreply.github.com>
2025-07-19 07:12:59 -0400

    integrated mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5001;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT);
+  connectDB();
 });
