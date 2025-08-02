235b5cbfdbadfe73ffbf2cc2594f0b76d2fd41e6
Nathan <87546582+nathandallas@users.noreply.github.com>
2025-08-02 12:16:25 -0400

    removed file for deploy

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://voluble-flax.vercel.app/"],
   },
 });
 
@@ -16,9 +16,9 @@ export function getReceiverSocketId(userId) {
 }
 
 // Store online users
-const userSocketMap = {}; 
+const userSocketMap = {};
 
-io.on("connection", (socket) => {
+io.on("connection", socket => {
   console.log("A user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
